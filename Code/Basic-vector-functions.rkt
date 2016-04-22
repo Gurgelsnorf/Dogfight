51ef28f73b179169de135d0da53acb29d3cd12ed
MarcusDahlqvist <marcusdahlqvist96@gmail.com>
2016-04-22 15:52:31 +0200

    Flyttade filer, redigerade vector-functions med enhetligt format
    Flyttade kollisionsdetekteringen till samma mapp så att de kan använda
    varandra lättare. Döpte om basic vector functions med - så att den kan
    kallas på.
    Redigerade basic-vector-functions så att funktionerna ser ut på samma
    sätt som det andra.
    Lade till funktionen $Vector_Length i basic vector functions.

---
+++ Code/Basic-vector-functions.rkt
@@ -0,0 +1,30 @@
+#lang racket
+(provide all-defined-out)
+
+;Vector multiplication with a real number and returns the new vector
+(define ($Vector_Multiplication real_numb vector)
+  (cons (* (car vector) real_numb) (* (cdr vector) real_numb)))
+
+;Vector addition with a another vector and returns the new vector
+(define ($Vector_Addition vector_1 vector_2)
+  (cons (+ (car vector_1) (car vector_2)) (+ (cdr vector_1) (cdr vector_2))))
+
+;Returns the length of a given vector
+(define ($Vector_Length vector)
+  (sqrt (+ (sqr (car vector)) (sqr (cdr vector)))))
+
+;Scalar of two vectors and return the value
+(define ($Vector_Dot_Product vector_1 vector_2)
+  (+ (* (car vector_1) (car vector_2)) (* (cdr vector_1) (cdr vector_2))))
+
+;Projection on a another vector does not give an ortagonal vector!
+(define ($Vector_Projection vector_proj vector_base)
+  ($Vector_Multiplication (/ ($Vector_Dot_Product vector_proj vector_base) (sqr ($Vector_Length vector_base))) vector_base)) 
+
+;Ortagonal element of the vector based on a another vector does not return the parallell element!
+(define ($Vector_Ortagonal vector_1 vector_base)
+  ($Vector_Addition vector_1 ($Vector_Multiplication ($Vector_Projection vector_1 vector_base) -1)))
+
+;Returns the Parallel vector and ortagonal vector as a pair in given order based on another vector
+(define ($Vector_Base_Split vector_1 vector_base)
+  (cons ($Vector_Projection vector_1 vector_base) ($Vector_Ortagonal vector_1 vector_base)))
