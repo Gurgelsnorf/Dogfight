eb2af0724f437bb8ae64069772e76c4c3573e449
Gurgelsnorf <gusda320@student.liu.se>
2016-05-10 14:08:38 +0200

    Added key-press and require problems with module
    inputcanvas.rkt.... *flying_untis*: unbound identifier error probably
    need help with this

--- Code/input-canvas.rkt
+++ Code/input-canvas.rkt
@@ -1,6 +1,11 @@
 #lang racket/gui
 (require "player-commands.rkt")
 (require "world-init.rkt")
+(require "collisions.rkt")
+(require "basic-procedures.rkt")
+(require "physics-engine.rkt")
+(require "flying-unit-rectangular.rkt")
+(require "graphics-engine-key-press.rkt")
 (provide (all-defined-out))
 
 
@@ -25,8 +30,18 @@
   (let
       ((£Key_Code (send event get-key-code)))
     (cond
-      ((eq? £Key_Code 'left) ($Increase_Angle *player_1* 1))
-      ((eq? £Key_Code 'right) ($Increase_Angle *player_1* -1)))))
+      ((eq? £Key_Code 'left) (begin
+                               ($Increase_Angle_Rotate *player_1* 1)
+                               (send *flying_units* refresh-now)))
+      ((eq? £Key_Code 'right) (begin
+                                ($Increase_Angle_Rotate *player_1* -1)
+                                (send *flying_units* refresh-now)))
+      ((eq? £Key_Code 'up) (begin
+                             ($Airplane_Full_Movement *player_1*)
+                             (send *flying_units* refresh-now)))
+      ((eq? £Key_Code 'down) (begin
+                               ($Move_All)
+                               (send *flying_units* refresh-now))))))
     
   
 
